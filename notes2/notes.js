3d1f8cb374560562a7afd36b093acd1e128392b5
aprocyk <48320987+aprocyk@users.noreply.github.com>
2020-01-19 19:15:31 +0100

    Notes v0.7
    Dodawanie i usuwanie notek działa

--- notes2/notes.js
+++ notes2/notes.js
@@ -1,64 +1,86 @@
 document.addEventListener('DOMContentLoaded', appStart)
 let notesArr = []
-let create 
-let ttle 
+let noteOut = []
+let create
+let ttle
 let desc
-let body  
+let body
+let clr
+//let dlt
 
-class Note{
-    constructor(title='', description=''){
+
+class Note {
+    constructor(title = '', description = '', color='') {
         this.title = title
         this.description = description
-        this.color = 'red'
+        this.color = color
         // let date = new Date()
         // this.created = date.toISOString()
         this.pinned = false
     }
+
 }
-function appStart(){
-    
+function appStart() {
+
     create = document.querySelector('#crt')
-        create.addEventListener('click', createNote)
+    create.addEventListener('click', createNote)
     ttle = document.querySelector('#title')
     desc = document.querySelector('#desc')
     body = document.querySelector('body')
-    fakeNote();
-    loadNotes();
+    clr = document.querySelector('#color')
     
     
+    if(localStorage.length > 0){
+        loadNotes();
+    }
+        
+
+
 }
-function fakeNote(){
+function createNote() {
     
-    const nt = new Note()
-    nt.title = 'Testowa notka'
-    nt.description = 'To jest testowa notka, po dodaniu nowej zostanie ona usunięta'
-    notesArr.push(nt)
-    localStorage.setItem('test', JSON.stringify(notesArr))
-    
-}
-function createNote(){
-    localStorage.removeItem('test')
-    const nt = new Note()
+    let nt = new Note()
     nt.title = ttle.value
     nt.description = desc.value
+    nt.color = clr.value
     notesArr.push(nt)
     localStorage.setItem('notes', JSON.stringify(notesArr))
+
 }
 
-function loadNotes(){
-    notesArr = JSON.parse(localStorage.getItem('notes'))
-    notesArr = JSON.parse(localStorage.getItem('test'))
-    notesArr.forEach(item => {
-        const div = document.createElement('div')
-        div.className = 'notka'
-        div.id = item.title
-        div.textContent = item.title+" "+item.description
-        div.style.background = item.color
-        body.appendChild(div)
-         
-    })
+function loadNotes() {
+        
+        notesArr = JSON.parse(localStorage.getItem('notes'))
+
+        notesArr.forEach((item,index) => {
+            const div = document.createElement('div')
+            const bar = document.createElement('div')
+            const content = document.createElement('div')
+            const del = document.createElement('button')
+            bar.className = 'bar'
+            bar.innerHTML = item.title
+            bar.style.background = 'lightgray'
+            del.className = 'delbut'
+            del.addEventListener('click',()=>{
+                console.log(index)
+                notesArr.splice(index, 1)
+                localStorage.setItem('notes', JSON.stringify(notesArr))
+                location.reload()
+                
+            })
+            content.className = 'content'
+            content.innerHTML = item.description 
+            content.style.background = item.color
+            div.id = item.title
+            div.className = 'notka' 
+            div.appendChild(bar)
+            bar.appendChild(del)
+            div.appendChild(content)
+            body.appendChild(div)
+        })
     
 }
+
 
 
 
